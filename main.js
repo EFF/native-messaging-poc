44c4495ca0f48c6a8968379a427d42d5d7c2a7f3
eff <frxpoulin@gmail.com>
2015-07-23 13:49:37 -0400

    update connection status and messaging method

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-var NATIVE_APP_NAME = "__enter_native_app_name__"
+var NATIVE_APP_NAME = "__enter_native_app_name__";
+var MESSAGING_METHODS_MAP = {
+	'native' : sendNativeMessage,
+	'socket' : sendMessageToSocket
+}
 
-var port;
+var port,
+	messagingMethod;
 
 function onDisconnected () {
 	console.log('DISCONNECTED FROM APP');
@@ -15,13 +20,42 @@ function sendNativeMessage(message) {
   port.postMessage(message);
 }
 
+function sendMessageToSocket(message){
+	console.log('WEBSOCKET NOT IMPLEMENTED YET', message)
+}
+
+function sendMessage(message){
+	messagingMethod.call(this, message);
+}
+
+function updateConnectionStatus() {
+	var statusElement = $('#status');
+
+	var status = navigator.onLine ? "Connecté" : "Déconnecté";
+	var className = navigator.onLine ? "text-success" : "text-danger";
+
+	statusElement.removeClass();
+	statusElement.text(status);
+	statusElement.addClass(className);
+}
+
+function updateMessagingMethodChanged(){
+	var _messagingMethod = $(this).val();
+	messagingMethod = MESSAGING_METHODS_MAP[_messagingMethod];
+}
+
 $(window).load(function(){
 	connectToNativeApp();
+	updateConnectionStatus();
 
 	$('#message-form').submit(function(e){
 		var message = $("#message-input").val()
-		sendNativeMessage(message);
+		sendMessage(message);
 		
 		e.preventDefault();
 	});
+
+	$('#messaging-method-select').change(updateMessagingMethodChanged);
+	window.addEventListener('online',  updateConnectionStatus);
+  	window.addEventListener('offline', updateConnectionStatus);
 });
