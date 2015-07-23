0b931afbdc29fc443b0ba8d4871a384a0886ada6
eff <frxpoulin@gmail.com>
2015-07-23 14:01:00 -0400

    convert indentation to space and remove background.js for eventPage.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-var NATIVE_APP_NAME = "__enter_native_app_name__";
-var MESSAGING_METHODS_MAP = {
-	'native' : sendNativeMessage,
-	'socket' : sendMessageToSocket
+var NATIVE_APP_NAME = "__enter_native_app_name__",
+    MESSAGING_METHODS_MAP = {
+    'native' : sendNativeMessage,
+    'socket' : sendMessageToSocket
 }
 
 var port,
-	messagingMethod;
+    messagingMethod;
 
 function onDisconnected () {
-	console.log('DISCONNECTED FROM APP');
+    console.log('DISCONNECTED FROM APP');
 }
 
 function connectToNativeApp() {
@@ -21,41 +21,41 @@ function sendNativeMessage(message) {
 }
 
 function sendMessageToSocket(message){
-	console.log('WEBSOCKET NOT IMPLEMENTED YET', message)
+    console.log('WEBSOCKET NOT IMPLEMENTED YET', message)
 }
 
 function sendMessage(message){
-	messagingMethod.call(this, message);
+    messagingMethod.call(this, message);
 }
 
 function updateConnectionStatus() {
-	var statusElement = $('#status');
+    var statusElement = $('#status');
 
-	var status = navigator.onLine ? "Connecté" : "Déconnecté";
-	var className = navigator.onLine ? "text-success" : "text-danger";
+    var status = navigator.onLine ? "Connecté" : "Déconnecté";
+    var className = navigator.onLine ? "text-success" : "text-danger";
 
-	statusElement.removeClass();
-	statusElement.text(status);
-	statusElement.addClass(className);
+    statusElement.removeClass();
+    statusElement.text(status);
+    statusElement.addClass(className);
 }
 
 function updateMessagingMethodChanged(){
-	var _messagingMethod = $(this).val();
-	messagingMethod = MESSAGING_METHODS_MAP[_messagingMethod];
+    var _messagingMethod = $(this).val();
+    messagingMethod = MESSAGING_METHODS_MAP[_messagingMethod];
 }
 
 $(window).load(function(){
-	connectToNativeApp();
-	updateConnectionStatus();
+    connectToNativeApp();
+    updateConnectionStatus();
 
-	$('#message-form').submit(function(e){
-		var message = $("#message-input").val()
-		sendMessage(message);
-		
-		e.preventDefault();
-	});
+    $('#message-form').submit(function(e){
+        var message = $("#message-input").val()
+        sendMessage(message);
+        
+        e.preventDefault();
+    });
 
-	$('#messaging-method-select').change(updateMessagingMethodChanged);
-	window.addEventListener('online',  updateConnectionStatus);
-  	window.addEventListener('offline', updateConnectionStatus);
+    $('#messaging-method-select').change(updateMessagingMethodChanged);
+    window.addEventListener('online',  updateConnectionStatus);
+      window.addEventListener('offline', updateConnectionStatus);
 });
